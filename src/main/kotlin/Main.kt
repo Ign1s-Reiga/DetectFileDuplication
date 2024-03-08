ffff7a23248410eabb0c44d9950dccf8c98bf13c
Ign1s-Reiga <remrem4862@gmail.com>
2024-03-09 01:59:31 +0900

    add folder path field

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,9 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -22,18 +26,35 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.nio.file.InvalidPathException
+import java.nio.file.Path
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var folderPath by remember { mutableStateOf("") }
     var filterExt by remember { mutableStateOf("") }
+    var detectingProgress by remember { mutableStateOf(0.0F) }
 
     val regex = Regex("")
 
     MaterialTheme {
         Box(modifier = Modifier.padding(horizontal = 20.dp).offset(y = 55.dp)) {
             Column {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text("Folder path: ")
+                    Spacer(modifier = Modifier.width(7.dp))
+                    TextField(
+                        value = folderPath,
+                        modifier = Modifier.fillMaxWidth(),
+                        isError = folderPath.isEmpty(),
+                        onValueChange = {
+                            folderPath = it
+                        },
+                        singleLine = true
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     Text("Extension filter (Optional): ", fontSize = 16.sp)
                     Spacer(modifier = Modifier.width(7.dp))
@@ -47,7 +68,23 @@ fun App() {
                         singleLine = true,
                     )
                 }
-                
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(
+                    onClick = {
+                        detectFileDuplication(folderPath)
+                                .also {
+
+                                }
+                    },
+                    enabled = folderPath.isNotEmpty(),
+                    modifier = Modifier.align(Alignment.End),
+                    content = { Text("Detect") }
+                )
+
+                LinearProgressIndicator(
+                    progress = detectingProgress,
+                    modifier = Modifier.fillMaxWidth(),
+                )
             }
 
         }
@@ -56,9 +93,23 @@ fun App() {
 
 fun main() = application {
     Window(
-        title = "Detect Duplicate File",
+        title = "Detect File Duplication",
         onCloseRequest = ::exitApplication
     ) {
         App()
     }
+}
+
+private fun detectFileDuplication(folderPath: String): Result {
+    try {
+        val path = Path.of(folderPath)
+    } catch (e: InvalidPathException) {
+        e.printStackTrace()
+        return Result.INVALID_PATH
+    }
+
+}
+
+enum class Result {
+    INVALID_PATH,
 }
