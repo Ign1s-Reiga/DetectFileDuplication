6211c354118fa710fc67ea6dbdab0d57f6beede9
Ign1s-Reiga <remrem4862@gmail.com>
2024-03-10 05:49:39 +0900

    state holder

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,10 +13,13 @@ import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Tab
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -26,15 +29,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.io.File
 import java.nio.file.InvalidPathException
 import java.nio.file.Path
 
 @Composable
 @Preview
 fun App() {
-    var folderPath by remember { mutableStateOf("") }
-    var filterExt by remember { mutableStateOf("") }
-    var detectingProgress by remember { mutableStateOf(0.0F) }
+    val mainViewState = remember { MainViewStateHolder() }
+    var dialogContent = "Title" to "Content"
 
     val regex = Regex("")
 
@@ -45,11 +48,11 @@ fun App() {
                     Text("Folder path: ")
                     Spacer(modifier = Modifier.width(7.dp))
                     TextField(
-                        value = folderPath,
+                        value = mainViewState.folderPath,
                         modifier = Modifier.fillMaxWidth(),
-                        isError = folderPath.isEmpty(),
+                        isError = mainViewState.folderPath.isEmpty(),
                         onValueChange = {
-                            folderPath = it
+                            mainViewState.folderPath = it
                         },
                         singleLine = true
                     )
@@ -59,11 +62,11 @@ fun App() {
                     Text("Extension filter (Optional): ", fontSize = 16.sp)
                     Spacer(modifier = Modifier.width(7.dp))
                     TextField(
-                        value = filterExt,
+                        value = mainViewState.extFilter,
                         modifier = Modifier.fillMaxWidth(),
                         placeholder = { Text("ex) .png, .jpg") },
                         onValueChange = {
-                            filterExt = it
+                            mainViewState.extFilter = it
                         },
                         singleLine = true,
                     )
@@ -71,22 +74,52 @@ fun App() {
                 Spacer(modifier = Modifier.height(8.dp))
                 Button(
                     onClick = {
-                        detectFileDuplication(folderPath)
+                        detectFileDuplication(mainViewState.folderPath)
                                 .also {
-
+                                    when(it) {
+                                        Result.NOT_EXISTS -> {
+                                            mainViewState.open()
+                                            dialogContent = "Such folder was not found" to "Please check folder path what you typed is correctly."
+                                        }
+                                        Result.INVALID_PATH -> {
+                                            mainViewState.open()
+                                            dialogContent = "Invalid folder path" to "Please check folder path what you typed is correctly."
+                                        }
+                                        else -> {}
+                                    }
                                 }
                     },
-                    enabled = folderPath.isNotEmpty(),
+                    enabled = mainViewState.folderPath.isNotEmpty(),
                     modifier = Modifier.align(Alignment.End),
                     content = { Text("Detect") }
                 )
+                Tab(
+                    selected = false,
+                    onClick = {},
+                )
 
                 LinearProgressIndicator(
-                    progress = detectingProgress,
+                    progress = mainViewState.detectingProgress,
                     modifier = Modifier.fillMaxWidth(),
                 )
             }
+        }
 
+        if (mainViewState.isDialogOpen) {
+            AlertDialog(
+                onDismissRequest = {},
+                buttons = {
+                    Column(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp)) {
+                        Button(
+                            onClick = { mainViewState.close() },
+                            modifier = Modifier.align(Alignment.End),
+                            content = { Text("OK") }
+                        )
+                    }
+                },
+                title = { Text(dialogContent.first) },
+                text = { Text(dialogContent.second) }
+            )
         }
     }
 }
@@ -100,16 +133,44 @@ fun main() = application {
     }
 }
 
-private fun detectFileDuplication(folderPath: String): Result {
+private fun detectFileDuplication(folderPathStr: String): Result {
+    val fileList = mutableListOf<File>()
+
     try {
-        val path = Path.of(folderPath)
+        val path = Path.of(folderPathStr)
+        val folder = path.toFile()
+
+        if (!folder.exists())
+            return Result.NOT_EXISTS
     } catch (e: InvalidPathException) {
         e.printStackTrace()
         return Result.INVALID_PATH
     }
-
+    return Result.SUCCESS
 }
 
 enum class Result {
+    NOT_EXISTS,
     INVALID_PATH,
+    SUCCESS
+}
+
+@Stable
+class MainViewStateHolder {
+    var folderPath by mutableStateOf("")
+        internal set
+    var extFilter by mutableStateOf("")
+        internal set
+    var detectingProgress by mutableFloatStateOf(0.0F)
+        internal set
+    var isDialogOpen by mutableStateOf(false)
+        private set
+
+    fun open() {
+        isDialogOpen = true
+    }
+
+    fun close() {
+        isDialogOpen = false
+    }
 }
